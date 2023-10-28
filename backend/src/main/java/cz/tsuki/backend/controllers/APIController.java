d2ef5d5b87a0c70cd1a942ac97470bef3f61a842
Tsuki-hime <karolina.sollova@seznam.cz>
2023-10-28 23:15:23 +0200

    updated hello endpoint

--- backend/src/main/java/cz/tsuki/backend/controllers/APIController.java
+++ backend/src/main/java/cz/tsuki/backend/controllers/APIController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-
-
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
