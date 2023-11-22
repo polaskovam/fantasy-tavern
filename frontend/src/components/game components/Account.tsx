c39a8ad0c494fac1b177442357d2e1ee177ed39a
Martina Polášková <104367983+polaskovam@users.noreply.github.com>
2023-11-22 18:34:38 +0100

    FT-26 create drinking account

--- frontend/src/components/game components/Account.tsx
+++ frontend/src/components/game components/Account.tsx
@@ -1,5 +1,60 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import {ListItemText, Paper} from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from '@mui/material/ListItem';
 
-function Account() {}
+function Account() {
+    return (
+        <Card sx={{width: 230, position: 'fixed', bottom: 10, right: 20, backgroundColor: "#f3dfc1"}}>
+            <Paper style={{padding: 16,  textAlign:"center", backgroundColor:"#f6e8d3"}}>
+                <Typography variant="h4" gutterBottom>
+                    2000
+                </Typography>
+                <Typography>
+                    Money Balance
+                </Typography>
+            </Paper>
+            <List style={{height: 180, overflowY: 'auto'}}>
+                <ListItem>
+                    <ListItemText
+                        primary="Margarita"
+                        secondary="5x"
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            textAlign: 'left'
+                        }}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                        primary="Tequilla"
+                        secondary="2x"
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            textAlign: 'left'
+                        }}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                        primary="Vodka"
+                        secondary="3x"
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            textAlign: 'left'
+                        }}
+                    />
+                </ListItem>
+
+            </List>
+
+        </Card>
+    )
+}
 
 export default Account;
--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Navbar from "../components/game components/Navbar";
 import Box from "@mui/material/Box";
-
+import Account from "../components/game components/Account";
 function GamePage() {
     return (
         <Box>
             <Navbar />
+            <Account />
 
 
         </Box>
