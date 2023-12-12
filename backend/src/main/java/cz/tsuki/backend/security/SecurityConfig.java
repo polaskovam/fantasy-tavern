6fd7599afee7f2a3d8407dfbaa13bdde6d7c012f
Karolína Šollová <karolina.sollova@arbes.com>
2023-12-12 15:15:39 +0100

    Access restriction set for roles

--- backend/src/main/java/cz/tsuki/backend/security/SecurityConfig.java
+++ backend/src/main/java/cz/tsuki/backend/security/SecurityConfig.java
@@ -2,10 +2,15 @@ package cz.tsuki.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +42,22 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(nathan, velorana, kasei);
+    }
+
+    //restricting access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(new AntPathRequestMatcher("/seed/**")).hasRole("BARTENDER")
+                        .requestMatchers(new AntPathRequestMatcher("/api/**")).hasAnyRole("BARTENDER", "DRUNK"));
+
+        //use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
 }
